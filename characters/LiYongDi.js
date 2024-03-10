bb2a1725daa370dee7b0199afa59d1c0b05de597
Louis Chou <93809518+LouisYuHung@users.noreply.github.com>
2024-03-11 01:37:40 +0800

    240311 世界觀建立

--- characters/LiYongDi.js
+++ characters/LiYongDi.js
@@ -1,4 +1,4 @@
-const LiYongDi = new Person('Li Yong Di');
+const LiYongDi = new Person('利詠笛');
 LiYongDi.setAttribute('age', 26);
 LiYongDi.setAttribute('gender_first', 1);
 LiYongDi.setAttribute('gender_second', 0);
